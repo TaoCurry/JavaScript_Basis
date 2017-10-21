bbf833838f3f043ef3fd8f6f1eed719ef1a1f1e0
e <493921441@qq.com>
2017-10-21 22:28:54 +0800

    JavaScript基本数据结构和变量

--- Data Types.js
+++ Data Types.js
@@ -56,3 +56,54 @@ isNaN(NaN); //true
 //这不是JavaScript的设计缺陷。浮点数在运算过程中会产生误差，因为计算机无法精确表示无限循环小数。要比较两个浮点数是否相等，只能计算它们之差的绝对值，看是否小于某个阈值：
 Math.abs(1 / 3 - (1 - 2 / 3)) < 0.00000001;   //true
 
+/*null && undefined*/
+/*null : null表示一个“空”的值，它和0以及空字符串''不同，0是一个数值，''表示长度为0的字符串，而null表示“空”。
+   在其他语言中，也有类似JavaScript的null的表示，例如Java也用null，Swift用nil，Python用None表示。但是，在JavaScript中，还有一个和null类似的undefined，它表示“未定义”。 undefined仅仅在判断函数参数是否传递的情况下有用。*/
+
+/* Array */
+/* 数组是一组按顺序排列的集合，集合的每个值称为元素。JavaScript的数组可以包括任意数据类型。 */
+var arr = [1, 2, 3.14, 'Hello', null, true];  //包含六个元素。
+new Array(1, 2, 3);   //通过使用函数Array()创建数组[1, 2, 3]
+//出于代码的可读性考虑，强烈建议直接使用[]。
+var arr = [1, 2, 3, 'hello', null, true];
+arr[0]; //使用索引访问数组的元素
+arr[1];
+arr[6]; //超出数组索引范围，返回undefined
+
+/* 对象　Object */
+//JavaScript的对象是一组由键-值组成的无序集合
+var person = {
+    name : 'Bob',
+    age : 20,
+    tags : ['js', 'web', 'mobile'],
+    city : 'Beijing',
+    hascar : true,
+    zipcoed : null
+};
+/*JavaScript对象的键都是字符串类型，值可以是任意数据类型。上述person对象一共定义了6个键值对，其中每个键又称为对象的属性，例如，person的name属性为'Bob'，zipcode属性为null。*/
+person.name;   //获取name属性
+person.city;   //对象名.属性名
+
+/* Variable 变量　*/
+/*
+    变量在JavaScript中就是用一个变量名表示，变量名是大小写英文、数字、$和_的组合，且不能用数字开头。变量名也不能是JavaScript的关键字，如if、while等。申明一个变量用var语句
+ */
+var a;  //申明了变量a,此时a的值为undefined
+var $b = 1;  //申明了变量$b,同时给$b赋值为１
+var s_007 = '007'; //
+var Anser = true;
+var t = null;
+
+/*
+    在JavaScript中，使用等号=对变量进行赋值。可以把任意数据类型赋值给变量，同一个变量可以反复赋值，而且可以是不同类型的变量，但是要注意只能用var申明一次
+ */
+var　a = 123;   //a的值为123
+a = 'abc';  //a变为字符串
+/* 这种变量本身类型不固定的语言称之为动态语言，与之对应的是静态语言。
+静态语言在定义变量时必须指定变量类型，如果赋值的时候类型不匹配，就会报错。 */
+
+/* strict模式　*/
+/*  JavaScript在设计之初，为了方便初学者学习，并不强制要求用var申明变量。这个设计错误带来了严重的后果：如果一个变量没有通过var申明就被使用，那么该变量就自动被申明为全局变量。使用var申明的变量则不是全局变量，它的范围被限制在该变量被申明的函数体内，同名变量在不同的函数体内互不冲突。
+    为了修补JavaScript这一严重设计缺陷，ECMA在后续规范中推出了strict模式，在strict模式下运行的JavaScript代码，强制通过var申明变量，未使用var申明变量就使用的，将导致运行错误。*/
+//启用strict模式的方法是在JavaScript代码的第一行写上：
+'use strict';
