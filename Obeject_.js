e0b1d2ea4b71363448af21cd8004d8ff1fefc1f3
e <493921441@qq.com>
2017-10-22 09:45:26 +0800

    JavaScript 对象

--- Obeject_.js
+++ Obeject_.js
@@ -1,6 +1,6 @@
 'use strict';
 /*JavaScript的对象是一种无序的集合数据类型，它由若干键值对组成。
-JavaScript的对象用于描述现实世界中的某个对象*/
+JavaScript的对象用于描述现实世界中的某个对象,例如，为了描述“小明”这个淘气的小朋友，我们可以用若干键值对来描述他*/
 var xiaoming = {
     name : 'xiaoming',
     birth : 1990,
