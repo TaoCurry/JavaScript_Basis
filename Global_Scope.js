4ba23ed348579eac8afc5241919f644d5ab4b5f5
e <493921441@qq.com>
2017-10-24 00:28:40 +0800

    JavaScript 全局作用域

--- Global_Scope.js
+++ Global_Scope.js
@@ -14,3 +14,17 @@ foo();  //直接调用foo()
 window.foo();   // 通过window.foo()调用
 
 // 直接调用的alert()函数其实也是window的一个变量
+//JavaScript实际上只有一个全局作用域。任何变量（函数也视为变量），如果没有在当前函数作用域中找到，就会继续往上查找，最后如果在全局作用域中也没有找到，则报ReferenceError错误
+
+/*名字空间*/
+//全局变量会绑定到window上，不同的JavaScript文件如果使用了相同的全局变量，或者定义了相同名字的顶层函数，都会造成命名冲突，并且很难被发现。
+//减少冲突的一个方法是把自己的所有变量和函数全部绑定到一个全局变量中
+
+
+var MYAPP　= {};  //唯一的全局变量
+MYAPP.name = 'mayapp';  //其他变量
+MYAPP.version = 1.0;    //
+MYAPP.foo = function () {   //其他函数
+    return 'foo';
+}
+// 把自己的代码全部放入唯一的名字空间MYAPP中，会大大减少全局变量冲突的可能。许多著名的JavaScript库都是这么干的：jQuery，YUI，underscore等等。
